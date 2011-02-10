9a4299596a0807eaec5fc483925797ab833b2952
Philip Forget <philipforget@gmail.com>
2011-02-10 11:43:19 -0500

    Modifed bootstrap and gitignore

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,6 @@ virtualenv --no-site-packages --distribute . &&
 # Activates the newly created virtualenv
 source ./bin/activate &&
 # Installs the requirements to the new virtualenv
-if [ -f ./requirements.txt ]; then
-	pip install -r ./requirements.txt 
+if [ -f ./requirements.pip ]; then
+	pip install -r ./requirements.pip
 fi
